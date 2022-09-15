b271af99160782c6baea6f1c69e9ff27b7fb93b6
Lucianna Tittle <lucianna.roset@gmail.com>
2022-09-15 12:54:59 -0500

    Add server script

--- src/components/MyMonsters.js
+++ src/components/MyMonsters.js
@@ -1,59 +1,60 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-function MyMonster({monster, exile }) {
-  
-
-    function handleClick () {
-        window.location.reload()
-    const exileMonster = monster.id
-    exile(exileMonster)
+function MyMonster({ monster, exile }) {
+  function handleClick() {
+    window.location.reload();
+    const exileMonster = monster.id;
+    exile(exileMonster);
   }
-//   function updateKids (){ 
-//     let newCount = parseInt(setKidAmount(kidAmount + 1))
-//     const newAmount = {"children": newCount}
-    
- 
-//          fetch(`http://localhost:3000/Colony/${monster.id}`, {
-//            method: 'PATCH',
-//            headers: {
-//              'Content-Type': 'application/json',
-//            },
-//            body: JSON.stringify(newAmount),
-//          })
-//             // .then(res => res.json())
-//             // .then(data => updateColonyAmount(data))
-//        }
- const [stateDocile, setDocile] = useState(!monster.docile)
+  //   function updateKids (){
+  //     let newCount = parseInt(setKidAmount(kidAmount + 1))
+  //     const newAmount = {"children": newCount}
 
- function docile () {
-    
-    setDocile(!stateDocile) 
-       
-      let newChange = {
-        "docile": stateDocile
-      }
-    fetch(`http://localhost:3000/Colony/${monster.id}`, {
-           method: 'PATCH',
-           headers: {
-             'Content-Type': 'application/json',
-           },
-           body: JSON.stringify(newChange),
-         })
-        //  .then (res => res.json())
-        //  .then(data => updateManner(data))
-}
+  //          fetch(`http://localhost:8000/Colony/${monster.id}`, {
+  //            method: 'PATCH',
+  //            headers: {
+  //              'Content-Type': 'application/json',
+  //            },
+  //            body: JSON.stringify(newAmount),
+  //          })
+  //             // .then(res => res.json())
+  //             // .then(data => updateColonyAmount(data))
+  //        }
+  const [stateDocile, setDocile] = useState(!monster.docile);
 
-  return <div>
-  <div className="image">
-    <img src = {monster.image} />
-  </div>
-    <div>  {monster.name}</div>
-    <div>  </div>
-    <div> 
-        <button onClick = {docile}>  {stateDocile ? "Docile" : "Aggressive"} </button> 
-        <button onClick = {handleClick}> Exile </button>
-     </div>
-</div>;
+  function docile() {
+    setDocile(!stateDocile);
+
+    let newChange = {
+      docile: stateDocile,
+    };
+    fetch(`http://localhost:8000/Colony/${monster.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newChange),
+    });
+    //  .then (res => res.json())
+    //  .then(data => updateManner(data))
+  }
+
+  return (
+    <div>
+      <div className="image">
+        <img src={monster.image} />
+      </div>
+      <div> {monster.name}</div>
+      <div> </div>
+      <div>
+        <button onClick={docile}>
+          {" "}
+          {stateDocile ? "Docile" : "Aggressive"}{" "}
+        </button>
+        <button onClick={handleClick}> Exile </button>
+      </div>
+    </div>
+  );
 }
 
 export default MyMonster;
