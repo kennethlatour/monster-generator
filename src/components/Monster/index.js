4a59d4353553731bdcb10f181e231e1672b52e73
Lucianna Tittle <lucianna.roset@gmail.com>
2022-09-16 09:28:50 -0500

    Add monster modal

--- src/components/Monster/index.js
+++ src/components/Monster/index.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import planet from "../../assets/planetIcon2.png";
+import { Modal } from "../Modal";
 import styles from "./styles/Monster.module.css";
 
 function Monster({ monster, colonyPost }) {
+  const [showModal, setShowModal] = useState(false);
   function handleClick() {
     const monsterobj = {
       name: monster.name,
@@ -11,13 +13,17 @@ function Monster({ monster, colonyPost }) {
     };
 
     colonyPost(monsterobj);
+    setShowModal(true);
   }
+
   return (
     <div className={styles["monster-container"]}>
+      <Modal onComplete={() => setShowModal(false)} visible={showModal}>
+        Monster Added
+      </Modal>
       <img className={styles.monsterimg} src={monster.image} />
       <p>
         <button className={styles.colonyBtn} onClick={handleClick}>
-          {" "}
           Add To Colony <img src={planet} />
         </button>
       </p>
